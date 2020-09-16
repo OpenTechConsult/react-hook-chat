9e24f18bdc5a896c84adf3c06912d0fa4a93b183
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-09-16 16:23:31 +0000

    Add text input for room name Add onChange event and implement the handleRoomChange event handler Add the roomName state using useState hook Add a Link component from react-router-dom to provide navigation to '/roomname'

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './home.styles.css';
 
 const Home = () => {
+
+    const [roomName, setRoomName] = React.useState("");
+
+    const handleRoomNameChange = (event) => {
+        setRoomName(event.target.value);
+    };
+
     return (
         <div>
-            <h3>I am Home component</h3>
+            <h3>Choose a chat room</h3>
+            <input type="text"
+                    placeholder="Room"
+                    value={roomName}
+                    onChange={handleRoomNameChange} />
+            <Link to={`/${roomName}`}>Join room</Link>
         </div>
-    )
+    );
 }
 
 export default Home;
